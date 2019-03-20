ca4fe0ef144b4318e4f2402614ca3cc7b69a6fac
king <mittalakshay6@gmail.com>
2019-03-20 19:58:38 +0530

    created new ImageViewActivity

--- app/src/main/java/com/example/ocr3/MainActivity.java
+++ app/src/main/java/com/example/ocr3/MainActivity.java
@@ -1,39 +1,30 @@
 package com.example.ocr3;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.os.Build;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.SparseIntArray;
-import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.google.firebase.ml.vision.document.FirebaseVisionDocumentText;
 import com.google.firebase.ml.vision.document.FirebaseVisionDocumentTextRecognizer;
-import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 import com.otaliastudios.cameraview.BitmapCallback;
 import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.PictureResult;
-import com.otaliastudios.cameraview.VideoResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,23 +32,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Button clickBtn;
 
-    Activity mainActivity;
-
     private String TAG = "MainActivity";
-
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
-    }
+    protected static PictureResult mPictureResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainActivity = this;
+
         camera = findViewById(R.id.camera);
         imageView = findViewById(R.id.imageView);
         clickBtn = findViewById(R.id.button);
@@ -67,42 +49,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPictureTaken(@NonNull PictureResult result) {
                 // A Picture was taken!
-                result.toBitmap(result.getSize().getWidth(), result.getSize().getHeight(), new BitmapCallback() {
-                    @Override
-                    public void onBitmapReady(@Nullable Bitmap bitmap) {
-                        imageView.setImageBitmap(bitmap);
-                        clickBtn.setVisibility(View.INVISIBLE);
-
-                        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
-                        FirebaseVisionDocumentTextRecognizer detector = FirebaseVision.getInstance()
-                                .getCloudDocumentTextRecognizer();
-                        detector.processImage(image)
-                                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionDocumentText>() {
-                                    @Override
-                                    public void onSuccess(FirebaseVisionDocumentText result) {
-                                        // Task completed successfully
-                                        // ...
-                                        String resultText = result.getText();
-                                        for (FirebaseVisionDocumentText.Block block: result.getBlocks()) {
-                                            String blockText = block.getText();
-                                            Log.d(TAG, blockText);
-                                            for (FirebaseVisionDocumentText.Paragraph paragraph: block.getParagraphs()) {
-                                                String paragraphText = paragraph.getText();
-                                                Log.d(TAG, paragraphText);
-                                            }
-                                        }
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Task failed with an exception
-                                        // ...
-                                    }
-                                });
-
-                    }
-                });
+                mPictureResult = result;
+                Intent intent = new Intent(MainActivity.this, ImageViewActivity.class);
+                startActivity(intent);
             }
         });
     }
